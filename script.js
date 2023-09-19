cadca14c807769c63c3365da16c9a3bc065a3950
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2023-09-19 14:59:42 -0300

    Finalização do projeto

--- script.js
+++ script.js
@@ -2,24 +2,6 @@ const main = document.querySelector("main");
 const root = document.querySelector(":root");
 const input = document.getElementById("input");
 const resultInput = document.getElementById("result");
-
-document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
-  charKeyBtn.addEventListener("click", function () {
-    const value = charKeyBtn.dataset.value;
-    input.value += value;
-  });
-});
-document.getElementById("clear").addEventListener("click", function () {
-  input.value = "";
-  input.focus();
-});
-document.getElementById("equal").addEventListener("click", calculate);
-
-function calculate() {
-  const result = eval(input.value);
-  resultInput.value = result;
-}
-
 const allowedKeys = [
   "(",
   ")",
@@ -42,6 +24,17 @@ const allowedKeys = [
   " ",
 ];
 
+document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
+  charKeyBtn.addEventListener("click", function () {
+    const value = charKeyBtn.dataset.value;
+    input.value += value;
+  });
+});
+document.getElementById("clear").addEventListener("click", function () {
+  input.value = "";
+  input.focus();
+});
+
 input.addEventListener("keydown", function (ev) {
   ev.preventDefault();
   if (allowedKeys.includes(ev.key)) {
@@ -56,6 +49,42 @@ input.addEventListener("keydown", function (ev) {
   }
 });
 
+document.getElementById("equal").addEventListener("click", calculate);
+
 function calculate() {
-  console.log("calculado");
+  resultInput.value = "ERROR";
+  resultInput.classList.add("error");
+  const result = eval(input.value);
+  resultInput.value = result;
+  resultInput.value = result;
+  resultInput.classList.remove("error");
 }
+document
+  .getElementById("copyToClipboard")
+  .addEventListener("click", function (ev) {
+    const button = ev.currentTarget;
+    if (button.innerText === "Copy") {
+      button.innerText = "Copied!";
+      button.classList.add("success");
+      navigator.clipboard.writeText(resultInput.value);
+    } else {
+      button.innerText = "Copy";
+      button.classList.remove("success");
+    }
+  });
+
+document.getElementById("themeSwitcher").addEventListener("click", function () {
+  if (main.dataset.theme === "dark") {
+    root.style.setProperty("--bg-color", "#fif5f9");
+    root.style.setProperty("--border-color", "#aaa");
+    root.style.setProperty("--font-color", "#212529");
+    root.style.setProperty("--primary-color", "#4682B4");
+    main.dataset.theme = "light";
+  } else {
+    root.style.setProperty("--bg-color", "#212529");
+    root.style.setProperty("--border-color", "#666");
+    root.style.setProperty("--font-color", "#f1f5f9");
+    root.style.setProperty("--primary-color", "#87cefa");
+    main.dataset.theme = "dark";
+  }
+});
